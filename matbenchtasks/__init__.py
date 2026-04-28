92e98a75d22fc2246458173b491348f4d3116b81
Rudra <einstien56789@gmail.com>
2026-04-28 18:42:54 +0530

    Suppress matminer impute warning spam

--- matbenchtasks/__init__.py
+++ matbenchtasks/__init__.py
@@ -1,4 +1,13 @@
 """TRIADS runners for remaining Matbench tasks."""
 
-__version__ = "0.1.0"
+import warnings
 
+
+warnings.filterwarnings(
+    "ignore",
+    message=r".*PymatgenData\(impute_nan=False\).*",
+    category=UserWarning,
+    module=r"matminer\.utils\.data",
+)
+
+__version__ = "0.1.0"
